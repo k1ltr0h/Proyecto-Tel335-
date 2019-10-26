3fc1786fc85dd26bc37fd1544227c1f73dd34e1a
Mario Araya F <mrcxp1@gmail.com>
2019-10-26 02:29:32 -0300

    Add user: ok, manden un post a url-proyecto/usuario/add con json {nombre:nombre-del-usuario}

--- functions/routes/user.js
+++ functions/routes/user.js
@@ -17,21 +17,17 @@ router.get("/", function(req, res){
         user_dict[doc.id]=doc.data();
         //res.send("<p>"+doc.id+" => "+JSON.stringify(doc.data())+"</p>");
       });
-      res.send(user_dict);
-      return snapshot;
+      return res.send(user_dict);
     }).catch(err => {
       console.log('Error getting documents', err);
     });
-    //console.log("Aquí estoy o.o");
 });
 
-router.post("/add", () =>{
+router.post("/add", (req, res) =>{
     //Agregar documento con id automatica
-    docRef.add({
-        nombre: 'Lalo'
-      }).then(ref => {
+    docRef.add(req.body).then(ref => {
         console.log('Added document with ID: ', ref.id);
-        return null;
+        return res.status(200).end();
       }).catch(error =>{
         console.log("Shuuuu, te me caiste :c ", error);
     });
